a7d38f36d456e7c83dc946052b53167d94fc93f3
Emily Gomez <emgomez@unomaha.edu>
2021-05-05 14:43:23 -0500

    refactor react app

---
+++ react-app/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = ()=>{
+    return(
+        <div>
+            <h1>Hello world</h1>
+            <h3>Personal website that will soon host projects I have completed</h3>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
