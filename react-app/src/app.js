43d7e68d1fe5a3bdb841578e59a9b22fa5755972
Emily Gomez <emgomez@unomaha.edu>
2021-06-11 10:50:40 -0500

    another attempt

--- react-app/src/app.js
+++ react-app/src/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import VerticalTabs from './navbar.js';
 
 const App = ()=>{
     return(
         <div>
+            <VerticalTabs></VerticalTabs>
             <h1>Hello world</h1>
             <h3>About Me</h3>
             <p>My name is Emily and I am from Nebraska.</p>
